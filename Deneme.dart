d2f9c5626eb09fe545a0e3a2eb99d3911b777ec6
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-03-30 17:23:43 +0300

    Deneme

---
+++ Deneme.dart
@@ -0,0 +1,13 @@
+/*class MyWidget extends StatefulWidget {
+@override
+MyWidgetState createState() => _MyWidgetState();
+
+class _MyWidgetState extends State<MyWidget> {
+@override
+Widget build(BuildContext context) {
+return Container(
+child: Text("Hello World")
+
+);
+
+}*/
