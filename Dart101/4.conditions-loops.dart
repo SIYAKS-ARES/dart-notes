d53fb9f1b6221eea33a0c569615bc111324b602d
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-05-03 14:31:41 +0300

    Conditions and Loops - Update

--- Dart101/4.conditions-loops.dart
+++ Dart101/4.conditions-loops.dart
@@ -1,5 +1,4 @@
-void main (){
-
+void main() {
   var number = 17;
 
   /*if (number > 3) {
@@ -41,35 +40,78 @@ void main (){
 } while (true);
 }*/
 
-switch (number) {
-  case 1:print("Number is 1");break;
-  case 2:print("Number is 2");break;
-  case 3:print("Number is 3");break;
-  case 4:print("Number is 4");break;
-  case 5:print("Number is 5");break;
-  case 6:print("Number is 6");break;
-  case 7:print("Number is 7");break;
-  case 8:print("Number is 8");break;
-  case 9:print("Number is 9");break;
-  case 10:print("Number is 10");break;
-  case 11:print("Number is 11");break;
-  case 12:print("Number is 12");break;
-  case 13:print("Number is 13");break;
-  case 14:print("Number is 14");break;
-  case 15:print("Number is 15");break;
-  case 16:print("Number is 16");break;
-  case 17:print("Number is 17");break;
-  case 18:print("Number is 18");break;
-  case 19:print("Number is 19");break;
-  case 20:print("Number is 20");break;
-  case 21:print("Number is 21");break;
-  default:print("none");
-}
+  switch (number) {
+    case 1:
+      print("Number is 1");
+      break;
+    case 2:
+      print("Number is 2");
+      break;
+    case 3:
+      print("Number is 3");
+      break;
+    case 4:
+      print("Number is 4");
+      break;
+    case 5:
+      print("Number is 5");
+      break;
+    case 6:
+      print("Number is 6");
+      break;
+    case 7:
+      print("Number is 7");
+      break;
+    case 8:
+      print("Number is 8");
+      break;
+    case 9:
+      print("Number is 9");
+      break;
+    case 10:
+      print("Number is 10");
+      break;
+    case 11:
+      print("Number is 11");
+      break;
+    case 12:
+      print("Number is 12");
+      break;
+    case 13:
+      print("Number is 13");
+      break;
+    case 14:
+      print("Number is 14");
+      break;
+    case 15:
+      print("Number is 15");
+      break;
+    case 16:
+      print("Number is 16");
+      break;
+    case 17:
+      print("Number is 17");
+      break;
+    case 18:
+      print("Number is 18");
+      break;
+    case 19:
+      print("Number is 19");
+      break;
+    case 20:
+      print("Number is 20");
+      break;
+    case 21:
+      print("Number is 21");
+      break;
+    default:
+      print("none");
+  }
 
-for (int i = 0; i < 5; i++) { 
-  print(i + 1);
-  i++;
-  } 
+  for (int i = 0; i < 5; i++) {
+    print(i + 1);
+    i++;
+  }
 
   /*void main (){
 
@@ -113,5 +155,4 @@ do {
   }
 } while (true);
 }*/
-
 }
